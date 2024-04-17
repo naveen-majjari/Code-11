b468854df87a7462c15cd9b4edab150ad7160ccc
naveen-majjari <newprince1506@gmail.com>
2024-04-17 11:33:40 -0500

    first commit

---
+++ backend/docker-files/database/createtable.sql
@@ -0,0 +1,16 @@
+CREATE TABLE admin (
+  admin_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  phone VARCHAR(50),
+  address TEXT
+);
+
+CREATE TABLE  users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW()
+);
